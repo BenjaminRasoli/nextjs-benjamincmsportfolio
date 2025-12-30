079a583435595ade37e468551a5c19082749f598
Benjamin Rasoli (student syh) <benjamin.rasoli@yh.nackademin.se>
2025-12-30 22:00:02 +0100

    Updated hamburger menu UI

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import { useState, useEffect } from "react";
 import { NavbarTypes, SocialMedia, NavLinks } from "@/types";
 import { urlFor } from "@/sanity/sanity.query";
 import { AnimatePresence, motion } from "framer-motion";
-import Hamburger from "hamburger-react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar({ navbar }: { navbar: NavbarTypes[] }) {
-  const [isHovered, setIsHovered] = useState(false);
   const [isOpen, setOpen] = useState(false);
 
   useEffect(() => {
@@ -41,21 +41,10 @@ function Navbar({ navbar }: { navbar: NavbarTypes[] }) {
   };
 
   return (
-    <div
-      className={clsx("z-20", "sticky", "w-full", {
-        "top-0": isOpen,
-        "pt-5": isOpen,
-        "top-5": !isOpen,
-        "px-5": !isOpen,
-      })}
-    >
+    <div className={clsx("z-20", "sticky", "w-full top-0 p-5")}>
       <div
         className={clsx(
-          "container max-w-[1000px] 2xl:max-w-[1500px] p-4 flex w-full items-start justify-between text-white rounded-lg transition-all",
-          {
-            "bg-transparent backdrop-blur-3xl": !isOpen,
-            "bg-transparent": isOpen,
-          }
+          "container max-w-[1000px] 2xl:max-w-[1500px] backdrop-blur-3xl p-4 flex w-full items-start justify-between text-white rounded-lg transition-all"
         )}
       >
         {navbar.map((item: NavbarTypes) => (
@@ -115,17 +104,11 @@ function Navbar({ navbar }: { navbar: NavbarTypes[] }) {
                 </div>
               ))}
             </div>
-            <div
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-              className="lg:hidden z-50 flex items-center"
-            >
-              <Hamburger
-                rounded
-                label="show menu"
-                color={isHovered ? "#4F46E5" : "white"}
-                toggled={isOpen}
-                toggle={setOpen}
+            <div className="lg:hidden z-50 flex items-center">
+              <RxHamburgerMenu
+                size={35}
+                aria-label="menu"
+                onClick={() => setOpen(true)}
               />
             </div>
           </motion.div>
@@ -134,55 +117,72 @@ function Navbar({ navbar }: { navbar: NavbarTypes[] }) {
 
       <AnimatePresence>
         {isOpen && (
-          <motion.div
-            initial={{ x: 200, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: 200, opacity: 0 }}
-            transition={{ duration: 0.6 }}
-            className="z-40 p-4 px-10 absolute top-0 w-full h-screen bg-secondary text-white flex flex-col justify-start"
-          >
-            {navbar.map((item: NavbarTypes) => (
-              <div className="pt-5" key={item._id}>
-                {item.links.map((link) => (
-                  <div className="pb-5" key={link._key}>
-                    <a
-                      className="font-bold relative hover:text-tertiary cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-500 before:absolute before:bg-tertiary before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-500 after:absolute after:bg-tertiary after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]"
-                      href={
-                        link.slug.current === "/#cv"
-                          ? `${generateFileUrl(link.file?.asset?._ref)}?dl=`
-                          : link.slug.current
-                      }
-                      download={
-                        link.slug.current === "/#cv" &&
-                        `${generateFileUrl(link.file?.asset?._ref)}?dl=`
-                      }
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="fixed inset-0 z-30 backdrop-blur-md bg-black/40"
+              onClick={() => setOpen(false)}
+            />
+            <motion.div
+              initial={{ x: 350 }}
+              animate={{ x: 0 }}
+              exit={{ x: 350 }}
+              transition={{ duration: 0.5 }}
+              className="z-40 p-4 px-10 absolute right-0 top-0 w-[300px] h-screen bg-gradient-to-b from-black to-secondary text-white flex flex-col justify-start"
+            >
+              {navbar.map((item: NavbarTypes) => (
+                <div className="pt-7" key={item._id}>
+                  <div className="absolute right-8 top-[50px]">
+                    <IoMdClose
+                      size={35}
+                      aria-label="menu"
                       onClick={() => setOpen(false)}
-                    >
-                      {link.label}
-                    </a>
+                    />
                   </div>
-                ))}
-                <div className="flex space-x-4 pt-5">
-                  {item.socialMedia.map((social: SocialMedia) => (
-                    <a
-                      key={social._key}
-                      href={social.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="pb-1 relative hover:opacity-20"
-                    >
-                      <Image
-                        src={urlFor(social.icon).url()}
-                        width={30}
-                        height={30}
-                        alt={social.platform}
-                      />
-                    </a>
+                  {item.links.map((link) => (
+                    <div className="pb-5" key={link._key}>
+                      <a
+                        className="font-bold relative hover:text-tertiary cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-500 before:absolute before:bg-tertiary before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-500 after:absolute after:bg-tertiary after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]"
+                        href={
+                          link.slug.current === "/#cv"
+                            ? `${generateFileUrl(link.file?.asset?._ref)}?dl=`
+                            : link.slug.current
+                        }
+                        download={
+                          link.slug.current === "/#cv" &&
+                          `${generateFileUrl(link.file?.asset?._ref)}?dl=`
+                        }
+                        onClick={() => setOpen(false)}
+                      >
+                        {link.label}
+                      </a>
+                    </div>
                   ))}
+                  <div className="flex space-x-4 pt-5">
+                    {item.socialMedia.map((social: SocialMedia) => (
+                      <a
+                        key={social._key}
+                        href={social.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="pb-1 relative hover:opacity-20"
+                      >
+                        <Image
+                          src={urlFor(social.icon).url()}
+                          width={30}
+                          height={30}
+                          alt={social.platform}
+                        />
+                      </a>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </motion.div>
+              ))}
+            </motion.div>
+          </>
         )}
       </AnimatePresence>
     </div>
